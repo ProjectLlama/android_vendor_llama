1f431ec28c84beb246c81d6e418c2b732b8fded2
Benjamin Gwynn <13gwynn@gmail.com>
2012-10-02 21:26:57 +0100

    Added maguro device configuration.

---
+++ build/maguro.mk
@@ -0,0 +1,32 @@
+#
+# SAMSUNG GALAXY NEXUS CONFIG
+#	MAINTAINED BY DUDEMAN1996
+#
+
+### Specify phone tech before including full_phone
+$(call inherit-product, vendor/llama/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GN-GSM
+
+# Boot animation
+BOOTANIMATION_NAME := XHDPI
+
+# Inherit some common Llama stuff.
+$(call inherit-product, vendor/llama/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/llama/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := maguro
+PRODUCT_NAME := cm_maguro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="projectllama/llama/maguro:4.1.1/llama/398337:user/release-keys" PRIVATE_BUILD_DESC="llama-user 4.1.1 JRO03C 398337 release-keys"
